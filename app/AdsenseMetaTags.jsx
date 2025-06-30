18bbb70311c49a4c48fb101f981c9c481fc96ffe
Saadmajid <128070661+saadkabhai@users.noreply.github.com>
2025-06-30 09:44:29 +0500

    script

--- app/AdsenseMetaTags.jsx
+++ app/AdsenseMetaTags.jsx
@@ -1,8 +1,12 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
       <meta name="google-adsense-account" content="ca-pub-4969686146359420" />
-      <script
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4969686146359420"
         crossOrigin="anonymous"
