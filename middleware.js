7fbe4c9648e8b58e84066f3bd0e5fc191bf1d697
Saadmajid <128070661+saadkabhai@users.noreply.github.com>
2025-07-03 22:56:15 +0500

    decrease edge request

--- middleware.js
+++ middleware.js
@@ -6,12 +6,16 @@ export async function middleware(request) {
     const cookieStore = cookies();
     const status = EncryptText.get(cookieStore.get('status')?.value || null);
     if (status !== 'LoggedIn') {
-        if (request.nextUrl.pathname.startsWith('/Balance') || request.nextUrl.pathname.startsWith('/Affiliate')|| request.nextUrl.pathname.startsWith('/Leaderboard')) {
+        if (request.nextUrl.pathname.startsWith('/Balance') || request.nextUrl.pathname.startsWith('/Affiliate') || request.nextUrl.pathname.startsWith('/Leaderboard')) {
             return NextResponse.redirect(new URL('/Login', request.url));
         }
     }
     return NextResponse.next();
 }
 export const config = {
-    matcher: ['/:path*'],
+    matcher: [
+        '/Balance/:path*',
+        '/Affiliate/:path*',
+        '/Leaderboard/:path*',
+    ],
 };
